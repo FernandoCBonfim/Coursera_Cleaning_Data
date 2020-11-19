62bce42d244578ca6b1293b8d8119a29ae936c27
FernandoCBonfim <fernando.cbonfim@gmail.com>
2020-11-19 16:10:13 -0300

    1

--- teste.R
+++ teste.R
@@ -1,5 +1,2 @@
-fileurl <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?accessType=DOWNLOAD"
-download.file(fileurl, destfile="~/cameras.csv") 
-list.files( " . /data" )
-getwd()
+
 
