9d23160832b08e27009d751e349c0fb15e8676e7
FernandoCBonfim <fernando.cbonfim@gmail.com>
2020-11-20 19:57:30 -0300

    1

--- reading.R
+++ reading.R
@@ -1,7 +1,25 @@
-Camera <- read.table("./data/cameras.csv",sep=",",header =TRUE)
+rm(list=ls())
+
+fileurl <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?accessType=DOWNLOAD"
+download.file(fileurl, destfile="cameras.csv")
+a <- read.csv("cameras.csv")
+View(a)
+
+Camera <- read.table("./data/camera.csv",sep=",",header =TRUE)
 head(Camera)
 View(Camera)
+
 library(xlsx)
-Pasta <- read.xlsx("Pasta1.xlsx",sheetIndex=1,header = TRUE)
+Pasta <- read.xlsx("./data/Pasta1.xlsx",sheetIndex=1,header = TRUE)
 head(Pasta)
-View(Pasta)
+
+library(XML)
+file2 <- "teste.xml"
+doc <- xmlTreeParse(file2,useInternal=TRUE)
+rootNode <- xmlRoot(doc)
+xmlName(rootNode)
+rootNode[[1]][[1]][[1]][[1]]
+
+library(jsonlite)
+jsonData <- fromJSON("https://api.github.com/users/jtleek/repos")
+names(jsonData)
---
+++ swirl.R
@@ -0,0 +1,5 @@
+library(swirl)
+swirl()
+Fernando Bonfim
+1
+fernando.cbonfim@gmail.com
